ecd5403c0a02a3c28b0e2b0d2d333da78103b401
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-05-13 20:58:56 -0500

    feat: added google font

---
+++ src/config/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Montserrat_Alternates } from "next/font/google";
+
+// Fuentes de google
+
+export const inter = Inter({ subsets: ["latin"] });
+export const titleFont = Montserrat_Alternates({ 
+    subsets: ["latin"],
+    weight: ['500','700']
+});
