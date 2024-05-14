bfcd34cfb8553a55c77c9c4a05b2b553228fac37
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-05-14 01:41:09 -0500

    feat: top menu

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,10 +1,12 @@
+import { TopMenu } from "@/components";
 
 export default function ShopLayout({ children }: {
     children: React.ReactNode;
 }) {
     return (
         <>
-            <main className="bg-red-500 min-h-screen">{children}</main>
+            <TopMenu/>
+            <main className="min-h-screen">{children}</main>
         </>
     );
 }
