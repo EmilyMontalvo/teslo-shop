43c04ef9f73f73ae5cd3607aca2d0f03714ade7d
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-05-15 00:39:35 -0500

    feat: products by gender screens

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,22 +1,34 @@
-import { notFound } from "next/navigation";
+import { ProductGrid, Title } from "@/components";
+import { Product, Gender } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 
-interface Props{
+interface Props {
   params: {
-    id: string
+    id: Gender;
   }
 }
 
+const seedProducts = initialData.products;
 
-export default function CategoryPage({params}:Props) {
+export default function CategoryPage({ params }: Props) {
 
-  const {id} = params;
-  if(id === 'kids'){
-    notFound()
+  const { id } = params;
+  const products = seedProducts.filter(product => product.gender == id)
+  const labels: Record<Gender, string> = {
+    'men': 'Hombres',
+    'women': 'Mujeres',
+    'kid': 'Niños',
+    'unisex': 'Todos'
   }
 
+  // if(id === 'kids'){
+  //   notFound()
+  // }
+
   return (
-    <div>
-      <h1>Category Page</h1>
-    </div>
+    <>
+      <Title title={`Artículos para ${(labels)[id]}`} subtitle="Todos los productos" className="mb-2" />
+      <ProductGrid products={products} />
+    </>
   );
 }
