32853454c5c8551adf2296a20f6208f1a75eebda
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-05-14 22:02:56 -0500

    feat: product grid item

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export * from "./ui/top-menu/TopMenu"; //Top Menu
 export * from './ui/title/Title';
 export * from './ui/not-found/PageNotFound'
 export * from './products/product-grid/ProductGrid'
+export * from './products/product-grid/ProductGridItem'
