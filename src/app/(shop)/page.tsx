0b4bda964939caac8bb03bc17350b13aa388121d
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-05-14 22:20:43 -0500

    feat: image animation

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -6,9 +6,9 @@ const products = initialData.products;
 export default function Home() {
   return (
     <>
-      <Title title="Tienda" subtitle="Todos los productos" className="mb-2"/>
-      <ProductGrid products={products}/>
-      
+      <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
+      <ProductGrid products={products} />
+
     </>
   );
 }
