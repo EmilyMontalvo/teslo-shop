01bac94a1810cc38d47908a1e7b0766d36f7ec1a
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-05-19 03:53:43 -0500

    feat: footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 
 export * from "./ui/top-menu/TopMenu"; 
 export * from './ui/title/Title';
+export * from './ui/footer/Footer';
 export * from './ui/not-found/PageNotFound'
 export * from './products/product-grid/ProductGrid'
 export * from './products/product-grid/ProductGridItem'
---
+++ src/components/ui/footer/Footer.tsx
@@ -0,0 +1,21 @@
+import { titleFont } from '@/config/fonts'
+import Link from 'next/link'
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='flex w-full justify-center text-xs mb-10'>
+      <Link href={'/'} className='mx-5'>
+        Privacidad y Legal
+      </Link>
+      <Link href={'/'}>
+        <span className={`${titleFont.className} antialiased font-bold`}>Teslo</span>
+        <span> | shop</span>
+        <span> © {new Date().getFullYear()}</span>
+      </Link>
+      <Link href={'/'} className='mx-5'>
+        Ubicaciones
+      </Link>
+    </div>
+  )
+}
