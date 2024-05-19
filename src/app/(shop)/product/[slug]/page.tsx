70a5167a4a0dc8b8e721d4cf461ec337304d060e
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-05-19 17:43:49 -0500

    feat: metadata

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -3,6 +3,7 @@ export const revalidate = 10080; //7 días
 import { getProductBySlug } from "@/actions";
 import { MobileSlideShow, QuantitySelector, SizeSelector, SlideShow, StockLabel } from "@/components";
 import { titleFont } from "@/config/fonts";
+import { Metadata, ResolvingMetadata } from "next";
 
 import { notFound } from "next/navigation";
 
@@ -10,6 +11,32 @@ import { notFound } from "next/navigation";
 interface Props {
   params: {
     slug: string
+  }
+
+}
+
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const id = params.slug;
+
+  // fetch data
+  const product = await getProductBySlug(params.slug);
+
+  // optionally access and extend (rather than replace) parent metadata
+  //const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: product?.title ?? 'Producto no encontrado' ,
+    description: product?.description ?? '',
+    openGraph: {
+      title: product?.title ?? 'Producto no encontrado',
+      description: product?.description ?? '',
+      images: [`/products/${product?.images[1]}`]
+    },
   }
 }
 
