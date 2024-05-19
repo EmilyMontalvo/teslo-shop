41ad0dd8228194587d584574de36cd30153943b8
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-05-19 09:26:42 -0500

    feat: seed products, categories and images

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,4 +1,3 @@
-import { create } from 'zustand';
 import { initialData } from './seed';
 import prisma from '../lib/prisma';
 
@@ -8,16 +7,63 @@ async function main() {
 
     // 1. Borrar registros previos
     // Así ejecuta todas de manera simultanea
-    await Promise.all([
+    //await Promise.all([
         prisma.productImage.deleteMany(),
         prisma.product.deleteMany(),
         prisma.category.deleteMany(),
-    ]);
+        //]);
 
-    //Así ejecuta una por una
-    //   await prisma.productImage.deleteMany();
-    //   await prisma.product.deleteMany();
-    //   await prisma.category.deleteMany();
+        //Así ejecuta una por una
+        await prisma.productImage.deleteMany();
+        await prisma.product.deleteMany();
+        await prisma.category.deleteMany();
+
+    const { categories, products } = initialData;
+
+
+    //2. Insertar categorías
+
+    const categoriesData = categories.map((name) => ({ name }));
+
+    await prisma.category.createMany({
+        data: categoriesData
+    });
+
+
+    const categoriesDB = await prisma.category.findMany();
+
+    const categoriesMap = categoriesDB.reduce((map, category) => {
+        map[category.name.toLowerCase()] = category.id;
+        return map;
+    }, {} as Record<string, string>); //<string=shirt, string=categoryID>
+
+
+
+    // Productos
+
+    products.forEach(async (product) => {
+
+        const { type, images, ...rest } = product;
+
+        const dbProduct = await prisma.product.create({
+            data: {
+                ...rest,
+                categoryId: categoriesMap[type]
+            }
+        })
+
+
+        // Images
+        const imagesData = images.map(image => ({
+            url: image,
+            productId: dbProduct.id
+        }));
+
+        await prisma.productImage.createMany({
+            data: imagesData
+        });
+
+    });
 
     console.log('Seed ejecutado correctamente');
 }
