e97c3d8bf4f3e4029b6ea1577fc0e28894bdb8f8
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-05-19 18:47:00 -0500

    feat: add error message

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -13,26 +13,42 @@ export const AddToCart = ({ product }: Props) => {
 
     const [size, setSize] = useState<Size | undefined>()
     const [quantity, setQuantity] = useState<number>(1)
+    const [posted, setPosted] = useState(false)
 
 
     const addToCart = () => {
-        if(!size) return null;
+        setPosted(true)
+        if (!size) return null;
 
     }
 
 
     return (
         <>
+
+
             {/* Selector de Tallas */}
             <SizeSelector selectedSize={size} availableSizes={product.sizes} onSizeChange={setSize} />
+
 
             {/* Selector de Cantidad */}
             <QuantitySelector quantity={quantity} onQuantityChanged={setQuantity} />
 
+            {/* Error message */}
+            {
+                posted && !size && (
+
+                    <p className='mt-5 text-red-500 text-xs'>Debe de seleccionar una talla*</p>
+
+                )
+            }
+
             {/* Button */}
             <button
-            onClick={addToCart}
-             className="btn-primary my-5">Agregar al Carrito</button>
+                onClick={addToCart}
+                className="btn-primary my-5">Agregar al Carrito
+            </button>
+            
 
         </>
     )
