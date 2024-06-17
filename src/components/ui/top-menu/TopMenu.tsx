9d64084a9c6792e3a0865e4fb00bce29962e79b9
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-06-17 04:25:56 -0500

    feat: empty cart

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -55,11 +55,11 @@ setLoaded(true)
                 </Link>
 
                 {/* Cart icon */}
-                <Link href={'/cart'}>
+                <Link href={(totalItemsInCart === 0 && loaded)?'/empty' :'/cart'}>
                     <div className='relative mx-2'>
                         {
                             loaded && totalItemsInCart > 0 && (
-                                <span className='absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white'>
+                                <span className='fade-in absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white'>
                                     {totalItemsInCart}
                                 </span>
                             )
